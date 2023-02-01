4f64ef303595e894ba7cd117466195193451606c
Phuc Hoang <phuchoang.inf@gmail.com>
2023-02-01 17:14:32 +0700

    Init server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(8080)
