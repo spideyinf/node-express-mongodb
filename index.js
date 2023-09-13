6ae6541bcbf1226d0f221e48353575431b23b483
Peter <phuchoang.inf@gmail.com>
2023-09-13 15:09:07 +0700

    Save progress

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+require('./models/User')
+require('./services/passport')
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+
+const keys = require('./config/keys')
+const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
+app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] }))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+authRoutes(app)
 
 const PORT = process.env.PORT || 8080
-app.listen(8080)
+app.listen(PORT)
